b9953c6f1198d3b67d39ba72fcb87fdb3e9231db
Jakub Hošek <hosj03@vse.cz>
2022-05-16 16:26:09 +0200

    Preceding work on which it will be build.

---
+++ gallery.js
@@ -0,0 +1,40 @@
+
+function openImageGallery() {
+    document.getElementById("Image_gallery").style.display = "block";
+}
+
+function closeImageGallery() {
+    document.getElementById("Image_gallery").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    // document.style.display = "none";
+    // var dots = document.getElementsByClassName("demo");
+    // var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    slides[slideIndex-1].style.display = "flex";
+    // dots[slideIndex-1].className += " active";
+    // captionText.innerHTML = dots[slideIndex-1].alt;
+}
